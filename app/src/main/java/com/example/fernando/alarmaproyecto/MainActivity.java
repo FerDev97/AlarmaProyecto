164202031833311c9dc9fbad5a7f3f0a02c39131
kornflakesguy <fer.aravalo1997@gmail.com>
2018-07-07 08:58:43 -0600

    Prueba fer

--- app/src/main/java/com/example/fernando/alarmaproyecto/MainActivity.java
+++ app/src/main/java/com/example/fernando/alarmaproyecto/MainActivity.java
@@ -30,11 +30,12 @@ public class MainActivity extends AppCompatActivity
 
 
     TextView text;
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
