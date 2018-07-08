af8ca75a5a3f9b1bcf987df670cb672cef8ab151
fherzt <franhdez97@hotmail.com>
2018-07-07 20:00:41 -0600

    Modificacion xd.

--- app/src/main/java/com/example/fernando/alarmaproyecto/AlertReceiver.java
+++ app/src/main/java/com/example/fernando/alarmaproyecto/AlertReceiver.java
@@ -3,14 +3,27 @@ package com.example.fernando.alarmaproyecto;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 /**
  * Created by FERNANDO on 6/7/2018.
  */
 
 public class AlertReceiver extends BroadcastReceiver {
+    public static String titulo, descripcion;
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        System.out.println("Ya es hora.");
 
+        Toast.makeText(context,"ya es hora!",Toast.LENGTH_SHORT).show();
+        //ahora va a vibrar
+        Vibrator vibrator=(Vibrator)context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(1500);
+        Intent intent2=new Intent(context,PruebaActivity.class);
+        context.startActivity(intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
     }
 }
--- app/src/main/java/com/example/fernando/alarmaproyecto/NotificationHelper.java
+++ app/src/main/java/com/example/fernando/alarmaproyecto/NotificationHelper.java
@@ -1,18 +1,57 @@
 package com.example.fernando.alarmaproyecto;
 
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 /**
  * Created by FERNANDO on 6/7/2018.
  */
 
 public class NotificationHelper extends ContextWrapper {
-    public static final String titulo="titulo";
-    public static final String mensaje="mensasje";
+    public static final String channelID="channel1";
+    public static final String channelName="channel 1";
+
+    private NotificationManager notificationManager;
 
     public NotificationHelper(Context base) {
         super(base);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O)
+            crear();
+    }
+
+
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void crear() {
+        NotificationChannel chanel=new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+        chanel.enableLights(true);
+        chanel.setLightColor(R.color.colorPrimary);
+        chanel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        getManager().createNotificationChannel(chanel);
+    }
+
+    public NotificationManager getManager() {
+        if(notificationManager==null) {
+            notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        return notificationManager;
+    }
+
+    public NotificationCompat.Builder getChannelNotification(String titulo, String descripcion) {
+        return new NotificationCompat.Builder(getApplicationContext(), channelID)
+                .setContentTitle(titulo)
+                .setContentText(descripcion)
+                .setSmallIcon(R.drawable.ic_check_circle_black_24dp);
     }
 
 
